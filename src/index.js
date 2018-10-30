cfa7810df1ba536cb975fa11b139730171184012
Abhay-D <dabhay3@gmail.com>
2018-10-30 15:54:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './styles/master.less';
+import App from './components/main/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
